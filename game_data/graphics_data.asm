fbd4d1191d5f16b724952468da979b855993a8d8
CupOfJoeCode <therealjoe2794@gmail.com>
2021-11-19 22:03:02 -0500

    Changed the palettes

--- game_data/graphics_data.asm
+++ game_data/graphics_data.asm
@@ -1,15 +1,15 @@
 PaletteData:
   ;background palette data
-  .byte $0F,$01,$29,$16
-  .byte $0F,$01,$29,$16
-  .byte $0F,$01,$29,$16
-  .byte $0F,$01,$29,$16  
+  .byte $21,$01,$29,$16
+  .byte $21,$01,$29,$16
+  .byte $21,$01,$29,$16
+  .byte $21,$01,$29,$16  
 
   ;sprite palette data
-  .byte $0F,$01,$21,$26
-  .byte $0F,$01,$21,$26
-  .byte $0F,$01,$21,$26
-  .byte $0F,$01,$21,$26  
+  .byte $21,$07,$27,$26
+  .byte $21,$07,$27,$26
+  .byte $21,$07,$27,$26
+  .byte $21,$07,$27,$26  
 
 SpriteData:
   .byte $08, $01, %00000000, $08 ; 
